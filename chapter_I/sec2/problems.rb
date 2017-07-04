804aeb62126ff3a8b8fe208fda595e5ea01889c6
QuangTD <tranduc811@gmail.com>
2017-07-05 03:51:22 +0700

    Chapter 2 - Problem 2.4 - Inversions counter (draft)

--- chapter_I/sec2/problems.rb
+++ chapter_I/sec2/problems.rb
@@ -79,3 +79,62 @@ class P22
     end
   end
 end
+
+# Inversion counter
+class P24
+  class << self
+    def merge(ar_a, p, q, r)
+      n = 0
+      n1 = q - p + 1
+      n2 = r - q
+      left = []
+      right = []
+
+      (0..(n1 - 1)).each do |i|
+        left[i] = ar_a[i + p]
+      end
+
+      (0..(n2 - 1)).each do |i|
+        right[i] = ar_a[i + q + 1]
+      end
+
+      i = 0
+      j = 0
+      (p..r).each do |k|
+        if i >= n1
+          while k <= r
+            ar_a[k] = right[j]
+            j += 1
+            k += 1
+          end
+          break
+        elsif j >= n2
+          while k <= r
+            ar_a[k] = left[i]
+            i += 1
+            k += 1
+          end
+          break
+        end
+
+        if left[i] <= right[j]
+          ar_a[k] = left[i]
+          i += 1
+        else
+          n += n2 - j + 1
+          ar_a[k] = right[j]
+          j += 1
+        end
+      end
+      n
+    end
+
+    def merge_sort(ar_a, p, r, count)
+      return unless p < r
+      q = (p + r) / 2
+      merge_sort(ar_a, p, q, count)
+      merge_sort(ar_a, q + 1, r, count)
+      count + merge(ar_a, p, q, r)
+    end
+  end
+end
