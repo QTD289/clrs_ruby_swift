4212105c195a57acd49f925c542889e936bc8466
QuangTD <tranduc811@gmail.com>
2017-08-16 22:26:13 +0700

    Part II - Chapter 6 - Heap sort

---
+++ Part_II/Chapter_6/heap_sort.rb
@@ -0,0 +1,60 @@
+# Heap sort
+class HeapSort
+  class << self
+    def parent(i)
+      (i - 1) / 2
+    end
+
+    def left(i)
+      2 * i + 1
+    end
+
+    def right(i)
+      2 * i + 2
+    end
+
+    def max_heapify(ar, i)
+      l = left(i)
+      r = right(i)
+
+      largest = if l < ar.size && ar[l] > ar[i]
+                  l
+                else
+                  i
+                end
+
+      largest = r if r < ar.size && ar[r] > ar[largest]
+
+      return ar if largest == i
+
+      temp = ar[i]
+      ar[i] = ar[largest]
+      ar[largest] = temp
+      max_heapify(ar, largest)
+    end
+
+    def build_max_heap(ar)
+      ((ar.size - 1) / 2).downto(0) do |i|
+        max_heapify(ar, i)
+      end
+    end
+
+    def sort(ar)
+      build_max_heap(ar)
+      heap_size = ar.size
+
+      (ar.size - 1).downto(0) do |i|
+        temp = ar[i]
+        ar[i] = ar[0]
+        ar[0] = temp
+
+        heap_size -= 1
+        ar_heap = max_heapify(ar.first(heap_size), 0)
+        (0..ar_heap.size - 1).each do |j|
+          ar[j] = ar_heap[j]
+        end
+      end
+      ar
+    end
+  end
+end
